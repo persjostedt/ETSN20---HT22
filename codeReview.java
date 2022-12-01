d9dcc7cc989889c1dc304329023e68714b90d3e4
persjostedt <119569380+persjostedt@users.noreply.github.com>
2022-12-01 13:03:12 +0100

    Add files via upload

--- codeReview.java
+++ codeReview.java
@@ -1,4 +1,5 @@
 package Lab4;
+
 import java.util.Scanner;
 import java.io.File;
 
@@ -8,22 +9,25 @@ public class codeReview {
 
 		Scanner scan = new Scanner(System.in);
 
+		// Försöker öppna namnet på en fil och hämta information om den.
+		// Om det går skickas ett FileNotFoundException
 		System.out.println("Skriv in namnet på filen du vill söka inuti: ");
-	
-		String fileName = scan.nextLine() + "txt";
-		
-		Scanner s = new Scanner(new File(fileName));	
-		
+		String fileName = scan.nextLine();
+		File file = new File(fileName);
+		Scanner s = new Scanner(file);
+
 		System.out.println("Skriv in vilket ord du letar efter: ");
-		
 		String search = scan.nextLine();
-	
-		while(s.hasNext()) {
-			String [] temp =s.nextLine().split(" ");
-			for(int i = 0; i<temp.length; i++) {
-				if(temp[i].equals(search)) {
+
+		// Söker igenom en linje text i taget. Delar upp varje rad av text i
+		// individuella ord.
+		// och söker igenom dem, hittar de en match printas hela raden.
+		while (s.hasNext()) {
+			String[] temp = s.nextLine().split(" ");
+			for (int i = 0; i < temp.length; i++) {
+				if (temp[i].equals(search)) {
 					System.out.println(s.nextLine());
-				}	
+				}
 			}
 		}
 	}
